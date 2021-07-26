92f7dae7d36dddfc655e9bfa6261e17d0e1f3fe1
BenG7447 <53404904+BenG7447@users.noreply.github.com>
2021-07-26 16:49:31 -0700

    Delete src directory

--- src/MONTHS.java
+++
@@ -1,5 +0,0 @@
-// Put all months in this file and import them as months array from MONTHS.values()
-
-public enum MONTHS {
-	January, February, March, April, May, June, July, August, September, October, November, December;
-}
